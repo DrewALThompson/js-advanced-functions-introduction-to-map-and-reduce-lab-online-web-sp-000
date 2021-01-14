8efbda40b7e387d8d7524adc2657dc5ce3906599
Drew Thompson <drewallynthompson@gmail.com>
2021-01-14 07:15:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function mapToNegativize(sourceArray){
   let array = [];
   for(const i in sourceArray){
-    console.log(sourceArray[i]);
     let neg = sourceArray[i] * -1;
     console.log(neg);
     array.push(neg);
